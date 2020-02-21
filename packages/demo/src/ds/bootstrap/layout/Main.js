fa4f44763a8956fa0b478354dec6cd4dd57bc13e
carolinholat <carolin.holat@gmail.com>
2020-02-21 10:28:35 +0100

    fix styling

--- packages/demo/src/ds/bootstrap/layout/Main.js
+++ packages/demo/src/ds/bootstrap/layout/Main.js
@@ -21,12 +21,12 @@ const BootstrapDashboard = ({children}) => {
         <BootstrapStyle/>
         <div className={clsx("container-fluid", "text-light")} >
             <NavBar/>
-            <div className={clsx("row")} style={{marginTop: "70px"}}>
+            <div className={clsx("row", "h-100")} style={{marginTop: "70px", height: "100% !important"}}>
                 <Sidebar/>
-                <div className={clsx("col-xs-12", "col-sm-9", "px-5", "overflow-auto")}>
+                <div className={clsx("col-xs-12", "col-sm-9", "px-5", "overflow-auto", "bg-secondary")} style={{height: "100% !important"}}>
                     <div className={clsx("row", "justify-content-center")}>
-                        <div className={clsx("col-md-10", "py-5", "bg-light")}>
-                            <div className={clsx("row", "shadow-sm", "p-3", "bg-info", "rounded")}>
+                        <div className={clsx("col-md-10", "py-5", "bg-secondary")}>
+                            <div className={clsx("row", "shadow-sm", "px-5", "py-5", "bg-info", "rounded")}>
                                 <h1>Bootstrap!</h1>
                                 {children}
                             </div>
--- packages/demo/src/ds/bootstrap/layout/Sidebar.js
+++ packages/demo/src/ds/bootstrap/layout/Sidebar.js
@@ -3,6 +3,13 @@ import clsx from "clsx";
 
 export default function Sidebar() {
 
-    return <div className={clsx("col-xs-12", "col-sm-3" , "py-5", "bg-info", "mh-100", "overflow-auto")}>SIDEBAR</div> ;
+    return <React.Fragment>
+        <ul className={clsx("col-xs-12", "col-sm-3", "py-5", "bg-info", "mh-100", "overflow-auto", "list-group", "list-group-flush")} style={{minHeight: "100% !important"}}>
+            <li className={clsx("list-group-item", "bg-info", "text-dark", "list-group-item-action", "text-center", "font-weight-bold")}>Material</li>
+            <li className={clsx("list-group-item", "bg-info", "text-dark", "list-group-item-action", "text-center", "font-weight-bold")}>Bootstrap</li>
+            <li className={clsx("list-group-item", "bg-info", "text-dark", "list-group-item-action", "text-center", "font-weight-bold")}>Pulse</li>
+            <li className={clsx("list-group-item", "bg-info", "text-dark", "list-group-item-action", "text-center", "font-weight-bold")}>Ant</li>
+        </ul>
+    </React.Fragment>;
 }
 
