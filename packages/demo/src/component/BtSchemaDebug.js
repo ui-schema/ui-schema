b103aae154f29fd91dc560c5adbb22fa7922e9d9
carolinholat <carolin.holat@gmail.com>
2020-02-22 16:48:47 +0100

    BtSchemaDebug styling bg-inherit

--- packages/demo/src/component/BtSchemaDebug.js
+++ packages/demo/src/component/BtSchemaDebug.js
@@ -8,21 +8,21 @@ const BtSchemaDebug = () => {
     const {store, schema, onChange} = useSchemaData();
 
     return <React.Fragment>
-        <div className={clsx("shadow-sm", "px-4", "py-4", "my-4", "bg-secondary")} /*style={{backgroundColor: "#002b36"}} */>
+        <div className={clsx("shadow-sm", "px-4", "py-4", "my-4", "bg-info", "border")} /*style={{backgroundColor: "#002b36"}} */>
             <ImmutableEditor data={store} onChange={(keys, value) => onChange(store.setIn(keys, value))} getVal={keys => store.getIn(keys)}
                              theme={{
-                                 base00: "bg-secondary",
+                                 base00: "bg-info",
                                  base0D: "text-light",
                                  base0B: "text-secondary",
                              }}/>
         </div>
-        <div className={clsx("shadow-sm", "px-4", "py-4", "my-4", "bg-secondary")} /*style={{backgroundColor: "#002b36"}} */>
+        <div className={clsx("shadow-sm", "px-4", "py-4", "my-4", "bg-info", "border")} /*style={{backgroundColor: "#002b36"}} */>
             <ImmutableEditor data={schema} onChange={() => console.log('not implemented')} getVal={keys => schema.getIn(keys)}
                              theme={{
-                                 base00: "bg-secondary",
-                                 base0D: "text-light",
+                                 base00: "bg-info",
+                                 base0D: "text-primary",
                                  base0B: "text-secondary",
-                             }}/>
+                             }} />
         </div>
     </React.Fragment>
 };
