8fd8ca56d1152244eb1c026d353fd813b48a2c34
Michael Becker <michael@bemit.codes>
2020-04-20 01:04:10 +0200

    publish scripts;

--- packer/deploy.sh
+++ packer/deploy.sh
@@ -10,34 +10,34 @@ git checkout -- packages/webpackPartialConfig.js
 cd ./packages
 
 cd ui-schema
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 cd ds-bootstrap
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 cd ds-material
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 cd material-code
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 cd material-color
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 cd material-pickers
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 cd material-richtext
-cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/
 cd ../
 
 #npm run release
-#npm run release -- --yes
+npm run release -- --yes
 
 rm .npmrc
