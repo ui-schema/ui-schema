d873c1f5ce9742ee363b6aa963c1edb9dfdcdb59
Michael Becker <michael@bemit.codes>
2020-04-20 01:28:06 +0200

    vup;

--- packages/ui-schema/src/Editor/SchemaEditor.d.ts
+++ packages/ui-schema/src/Editor/SchemaEditor.d.ts
@@ -2,7 +2,7 @@ import * as React from "react"
 import { EditorContext, EditorStoreContext } from "./EditorStore"
 
 export interface SchemaEditorProviderProps extends EditorContext, EditorStoreContext {
-    children: []
+    children?: React.ReactNode
 }
 
 export function SchemaEditorProvider<P extends SchemaEditorProviderProps>(props: P): React.Component
