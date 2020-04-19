bf1d382b154b49425a8b2bb1dc1d6fda74a1627c
Michael Becker <michael@bemit.codes>
2020-04-19 23:16:12 +0200

    fix publish;

--- packer/deploy.sh
+++ packer/deploy.sh
@@ -2,6 +2,9 @@
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >.npmrc
 
+# todo: not manual resetting the file here
+git checkout -- packages/webpackPartialConfig.js
+
 npm run release -- --yes
 
 rm .npmrc
