ab6c7686de7c3f95de5a897920b66f4df7fb0878
Michael Becker <michael@bemit.codes>
2020-04-20 00:39:51 +0200

    publish scripts;

--- packer/deploy.sh
+++ packer/deploy.sh
@@ -5,7 +5,37 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >.npmrc
 # todo: not manual resetting the file here
 git checkout -- packages/webpackPartialConfig.js
 
-npm run release
+cd ../packages
+
+cd ui-schema
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+cd ds-bootstrap
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+cd ds-material
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+cd material-code
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+cd material-color
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+cd material-pickers
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+cd material-richtext
+cp package.json build/ && cp package-lock.json build/ &&  cp README.md build/ && npm publish build
+cd ../
+
+#npm run release
 #npm run release -- --yes
 
 rm .npmrc
