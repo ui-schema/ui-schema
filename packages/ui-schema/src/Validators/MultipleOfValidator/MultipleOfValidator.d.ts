78af5783884f474cbbaad9380cf7cb443dae919e
carolinholat <carolin.holat@gmail.com>
2020-07-26 13:27:05 +0200

    update;

--- packages/ui-schema/src/Validators/MultipleOfValidator/MultipleOfValidator.d.ts
+++ packages/ui-schema/src/Validators/MultipleOfValidator/MultipleOfValidator.d.ts
@@ -1,16 +1,20 @@
-import { OrderedMap } from 'immutable'
-import { validatorPlugin } from '../../Validators/validate'
+import { OrderedMap, List } from 'immutable'
+import { errors, schema } from '@ui-schema/ui-schema/CommonTypings'
 
 export type ERROR_MULTIPLE_OF = string
 
-export interface validateMultipleOfProps {
-    type: string
-    schema: OrderedMap<{}, undefined>
-    value: any
-}
+export function validateMultipleOf(type: string, schema: OrderedMap<{}, undefined>, value: any): boolean
 
-export function validateMultipleOf(props: validateMultipleOfProps): boolean
-
-// tslint:disable-next-line:no-empty-interface
-export interface multipleOfValidator extends validatorPlugin {
+export interface multipleOfValidator {
+    validate: (
+        {schema, value, errors, valid}: {
+            schema: schema
+            value: any
+            errors: errors
+            valid: boolean
+        }
+    ) => {
+        errors: List<any>,
+        valid: boolean
+    }
 }
