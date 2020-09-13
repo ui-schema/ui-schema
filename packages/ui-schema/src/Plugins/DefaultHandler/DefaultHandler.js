54451c51e25897ec04e254e7ad19dc911755c72c
Michael Becker <michael@bemit.codes>
2020-09-13 20:08:45 +0200

    fix DefaultHandler behaviour with `required` deleteOnEmpty #61;
    it was not possible to e.g. unselect all items in an array which has a `default` and is `required`. Unselecting the last resulted in setting the default again. Now the default is only set again when the storeKeys are changing;

--- packages/ui-schema/src/Plugins/DefaultHandler/DefaultHandler.js
+++ packages/ui-schema/src/Plugins/DefaultHandler/DefaultHandler.js
@@ -1,24 +1,31 @@
-import React from "react";
-import {NextPluginRenderer} from "../../EditorPluginStack";
-import {updateValue} from "../../EditorStore";
+import React from 'react';
+import {NextPluginRenderer} from '../../EditorPluginStack';
+import {updateValue} from '../../EditorStore';
 
 const DefaultValueHandler = ({defaultVal, ...props}) => {
+    const storeKeysPrev = React.useRef(undefined);
     const [defaultHandled, setDefaultHandled] = React.useState(false);
     const {onChange, storeKeys} = props;
     let {value} = props;
+
+    const sameStoreKeys = storeKeysPrev.current && storeKeysPrev.current.equals(storeKeys);
+
+    if(!sameStoreKeys) {
+        storeKeysPrev.current = storeKeys;
+    }
+
+    React.useEffect(() => {
+        if(defaultHandled && !sameStoreKeys) {
+            setDefaultHandled(false);
+        }
+    }, [onChange, sameStoreKeys, defaultVal]);
 
     React.useEffect(() => {
         if(typeof value === 'undefined' && !defaultHandled) {
             setDefaultHandled(true);
             onChange(updateValue(storeKeys, defaultVal));
         }
-    }, [onChange, storeKeys, defaultVal, value, defaultHandled]);
-
-    React.useEffect(() => {
-        if(defaultHandled) {
-            setDefaultHandled(false);
-        }
-    }, [onChange, storeKeys, defaultVal]);
+    }, [onChange, sameStoreKeys, defaultVal, value, defaultHandled]);
 
     let nextValue = value;
     if(typeof value === 'undefined' && !defaultHandled) {
@@ -29,7 +36,7 @@ const DefaultValueHandler = ({defaultVal, ...props}) => {
 };
 
 const DefaultHandler = (props) => {
-    const {schema,} = props;
+    const {schema} = props;
 
     let defaultVal = schema.get('default');
 
