12a7b78b5bb707c840931b11838ae822c2d27137
Michael Becker <michael@bemit.codes>
2020-09-15 10:51:13 +0200

    switch to lerna-packer #32

--- packerConfig.js
+++ packerConfig.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const {buildExternal} = require('./packer/tools');
+//const {buildExternal} = require('./packer/tools');
+const {buildExternal, packer} = require('lerna-packer');
 
 const apps = {
     demo: {
@@ -21,8 +22,8 @@ const apps = {
         dist: path.resolve(__dirname, 'dist', 'docs'),
         servedPath: '/',
         vendors: ['react-error-boundary', 'immutable', '@material-ui/core', '@material-ui/icons'],
-        copy: [{from: path.resolve(__dirname, 'schema'), to: path.resolve(__dirname, 'dist', 'docs', 'schema')}]
-    }
+        copy: [{from: path.resolve(__dirname, 'schema'), to: path.resolve(__dirname, 'dist', 'docs', 'schema')}],
+    },
 };
 
 const packages = {
@@ -32,8 +33,8 @@ const packages = {
         name: '@ui-schema/ui-schema',
         root: path.resolve(__dirname, 'packages', 'ui-schema'),
         entry: path.resolve(__dirname, 'packages', 'ui-schema/src/'),
-        react: buildExternal("react"),
-        "react-dom": buildExternal("react-dom"),
+        react: buildExternal('react'),
+        'react-dom': buildExternal('react-dom'),
     },
     dsMaterial: {
         // noClean: true,
@@ -41,84 +42,83 @@ const packages = {
         root: path.resolve(__dirname, 'packages', 'ds-material'),
         entry: path.resolve(__dirname, 'packages', 'ds-material/src/'),
         externals: {
-            "@ui-schema/ui-schema": buildExternal("@ui-schema/ui-schema"),
-            "@material-ui/core": buildExternal("@material-ui/core"),
-            "@material-ui/icons": buildExternal("@material-ui/icons"),
-            react: buildExternal("react"),
-            "react-dom": buildExternal("react-dom"),
-        }
+            '@ui-schema/ui-schema': buildExternal('@ui-schema/ui-schema'),
+            '@material-ui/core': buildExternal('@material-ui/core'),
+            '@material-ui/icons': buildExternal('@material-ui/icons'),
+            react: buildExternal('react'),
+            'react-dom': buildExternal('react-dom'),
+        },
     },
     dsBootstrap: {
         name: '@ui-schema/ds-bootstrap',
         root: path.resolve(__dirname, 'packages', 'ds-bootstrap'),
         entry: path.resolve(__dirname, 'packages', 'ds-bootstrap/src/'),
         externals: {
-            "@ui-schema/ui-schema": buildExternal("@ui-schema/ui-schema"),
-            react: buildExternal("react"),
-            "react-dom": buildExternal("react-dom"),
-        }
+            '@ui-schema/ui-schema': buildExternal('@ui-schema/ui-schema'),
+            react: buildExternal('react'),
+            'react-dom': buildExternal('react-dom'),
+        },
     },
     materialPickers: {
         name: '@ui-schema/material-pickers',
         root: path.resolve(__dirname, 'packages', 'material-pickers'),
         entry: path.resolve(__dirname, 'packages', 'material-pickers/src/'),
         externals: {
-            "@ui-schema/ui-schema": buildExternal("@ui-schema/ui-schema"),
-            "@ui-schema/ds-material": buildExternal("@ui-schema/ds-material"),
-            "@material-ui/core": buildExternal("@material-ui/core"),
-            "@material-ui/icons": buildExternal("@material-ui/icons"),
-            react: buildExternal("react"),
-            "react-dom": buildExternal("react-dom"),
-        }
+            '@ui-schema/ui-schema': buildExternal('@ui-schema/ui-schema'),
+            '@ui-schema/ds-material': buildExternal('@ui-schema/ds-material'),
+            '@material-ui/core': buildExternal('@material-ui/core'),
+            '@material-ui/icons': buildExternal('@material-ui/icons'),
+            react: buildExternal('react'),
+            'react-dom': buildExternal('react-dom'),
+        },
     },
     materialRichtext: {
         name: '@ui-schema/material-richtext',
         root: path.resolve(__dirname, 'packages', 'material-richtext'),
         entry: path.resolve(__dirname, 'packages', 'material-richtext/src/'),
         externals: {
-            "@ui-schema/ui-schema": buildExternal("@ui-schema/ui-schema"),
-            "@ui-schema/ds-material": buildExternal("@ui-schema/ds-material"),
-            "@material-ui/core": buildExternal("@material-ui/core"),
-            "@material-ui/icons": buildExternal("@material-ui/icons"),
-            react: buildExternal("react"),
-            "react-dom": buildExternal("react-dom"),
-            draftJs: buildExternal("draft-js"),
-            draftJsPluginsEditor: buildExternal("draft-js-plugins-editor"),
-        }
+            '@ui-schema/ui-schema': buildExternal('@ui-schema/ui-schema'),
+            '@ui-schema/ds-material': buildExternal('@ui-schema/ds-material'),
+            '@material-ui/core': buildExternal('@material-ui/core'),
+            '@material-ui/icons': buildExternal('@material-ui/icons'),
+            react: buildExternal('react'),
+            'react-dom': buildExternal('react-dom'),
+            draftJs: buildExternal('draft-js'),
+            draftJsPluginsEditor: buildExternal('draft-js-plugins-editor'),
+        },
     },
     materialCode: {
         name: '@ui-schema/material-code',
         root: path.resolve(__dirname, 'packages', 'material-code'),
         entry: path.resolve(__dirname, 'packages', 'material-code/src/'),
         externals: {
-            "@ui-schema/ui-schema": buildExternal("@ui-schema/ui-schema"),
-            "@ui-schema/ds-material": buildExternal("@ui-schema/ds-material"),
-            "@material-ui/core": buildExternal("@material-ui/core"),
-            "@material-ui/icons": buildExternal("@material-ui/icons"),
-            react: buildExternal("react"),
-            "react-dom": buildExternal("react-dom"),
-            reactCodemirror2: buildExternal("react-codemirror2"),
-            codemirror: buildExternal("codemirror"),
-        }
+            '@ui-schema/ui-schema': buildExternal('@ui-schema/ui-schema'),
+            '@ui-schema/ds-material': buildExternal('@ui-schema/ds-material'),
+            '@material-ui/core': buildExternal('@material-ui/core'),
+            '@material-ui/icons': buildExternal('@material-ui/icons'),
+            react: buildExternal('react'),
+            'react-dom': buildExternal('react-dom'),
+            reactCodemirror2: buildExternal('react-codemirror2'),
+            codemirror: buildExternal('codemirror'),
+        },
     },
     materialColor: {
         name: '@ui-schema/material-color',
         root: path.resolve(__dirname, 'packages', 'material-color'),
         entry: path.resolve(__dirname, 'packages', 'material-color/src/'),
         externals: {
-            "@ui-schema/ui-schema": buildExternal("@ui-schema/ui-schema"),
-            "@ui-schema/ds-material": buildExternal("@ui-schema/ds-material"),
-            "@material-ui/core": buildExternal("@material-ui/core"),
-            "@material-ui/icons": buildExternal("@material-ui/icons"),
-            react: buildExternal("react"),
-            "react-dom": buildExternal("react-dom"),
-            materialUiColorPicker: buildExternal("material-ui-color-picker")
-        }
+            '@ui-schema/ui-schema': buildExternal('@ui-schema/ui-schema'),
+            '@ui-schema/ds-material': buildExternal('@ui-schema/ds-material'),
+            '@material-ui/core': buildExternal('@material-ui/core'),
+            '@material-ui/icons': buildExternal('@material-ui/icons'),
+            react: buildExternal('react'),
+            'react-dom': buildExternal('react-dom'),
+            materialUiColorPicker: buildExternal('material-ui-color-picker'),
+        },
     },
 };
 
+//const packer = require('./packer/packer')
+//packer(apps, packages)
 
-const packer = require('./packer/packer')
-
-packer(apps, packages)
-
+packer(apps, packages, __dirname);
