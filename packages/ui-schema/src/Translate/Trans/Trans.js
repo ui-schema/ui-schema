fb63e07ab9e746caee77a73c585dd8e775c4920b
Michael Becker <michael@bemit.codes>
2021-03-06 00:03:01 +0100

    optimize `Trans` performance, no-rerender on widget rendering
    was problematic especially for bigger lists of elements with a lot of labels

--- packages/ui-schema/src/Translate/Trans/Trans.js
+++ packages/ui-schema/src/Translate/Trans/Trans.js
@@ -4,15 +4,29 @@ import {useUIMeta} from '../../UIStore';
 /**
  * Translation component, supports dot strings, dictionary can be mixed strings, string functions and function components as translation
  */
-export const Trans = ({text, context, schema, fallback}) => {
-    const {t} = useUIMeta();
+let TransBase = ({text, context, schema, fallback, t}) => {
     const Translated = t(text, context, schema);
 
-    return !Translated ?
-        fallback || text :
+    return !Translated && Translated !== 0 && Translated !== '0' ?
+        typeof fallback !== 'undefined' && fallback !== '' ? fallback : text :
         typeof Translated === 'string' || typeof Translated === 'number' || typeof Translated === 'object' ?
             Translated :
             typeof Translated === 'function' ?
                 <Translated/> :
                 text
+};
+
+// here this memo is enough, doesn't need immutable compatible memo
+TransBase = React.memo(TransBase)
+
+export const Trans = ({text, context, schema, fallback}) => {
+    const {t} = useUIMeta();
+
+    return <TransBase
+        text={text}
+        context={context}
+        schema={schema}
+        fallback={fallback}
+        t={t}
+    />
 };
