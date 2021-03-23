f59f95f5010fd52903c578e21a8e0703e016e9c0
Michael Becker <michael@bemit.codes>
2021-03-23 10:10:51 +0100

    material-code typings

--- packages/material-code/src/CodeSelectable/CodeSelectable.d.ts
+++ packages/material-code/src/CodeSelectable/CodeSelectable.d.ts
@@ -1,8 +1,4 @@
 import * as React from 'react'
-import { WidgetProps } from '@ui-schema/ui-schema/Widget'
+import { WidgetProps } from '@ui-schema/ui-schema'
 
-export interface CodeProps extends WidgetProps {
-    value: string
-}
-
-export function Code<P extends CodeProps>(props: P): React.ReactElement<P>
+export function CodeSelectable<P extends WidgetProps>(props: P): React.ReactElement
