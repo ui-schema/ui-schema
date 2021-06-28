cb106e1604f2547d0b5b6cb32d055611cf4c9db0
Michael Becker <michael@bemit.codes>
2021-06-28 19:54:00 +0200

    optimize ExtractStorePlugin #149

--- packages/ui-schema/src/Plugins/ExtractStorePlugin/ExtractStorePlugin.tsx
+++ packages/ui-schema/src/Plugins/ExtractStorePlugin/ExtractStorePlugin.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import { memo } from '@ui-schema/ui-schema/Utils/memo'
-import { extractValue, WithValue } from '@ui-schema/ui-schema/UIStore'
-import { getNextPlugin, PluginProps } from '@ui-schema/ui-schema/PluginStack'
+import { extractValue } from '@ui-schema/ui-schema/UIStore'
+import { NextPluginRendererMemo, PluginProps } from '@ui-schema/ui-schema/PluginStack'
 
-const ExtractStorePluginBase: React.ComponentType<PluginProps & WithValue> = ({currentPluginIndex, ...p}) => {
-    const next = currentPluginIndex + 1
-    const Plugin = getNextPlugin(next, p.widgets)
-    return <Plugin {...p} currentPluginIndex={next}/>
-}
-export const ExtractStorePlugin: React.ComponentType<PluginProps> = extractValue(memo(ExtractStorePluginBase))
+export const ExtractStorePlugin: React.ComponentType<PluginProps> = extractValue(NextPluginRendererMemo)
